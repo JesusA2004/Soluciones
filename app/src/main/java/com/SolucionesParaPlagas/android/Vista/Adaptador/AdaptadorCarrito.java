297b0763f934e98110c5d0a29c80596645faba4c
Jesus <amjo220898@upemor.edu.mx>
2024-09-02 17:52:02 -0600

    Cambios hechos el 02/09/2024

--- app/src/main/java/com/SolucionesParaPlagas/android/Vista/Adaptador/AdaptadorCarrito.java
+++ app/src/main/java/com/SolucionesParaPlagas/android/Vista/Adaptador/AdaptadorCarrito.java
@@ -1,52 +1,70 @@
 package com.SolucionesParaPlagas.android.Vista.Adaptador;
 
-import android.view.LayoutInflater;
+import com.example.sol.R;
+import java.util.HashMap;
+import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
-
+import android.view.LayoutInflater;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
-
+import com.SolucionesParaPlagas.android.Controlador.ControladorImagenes;
+import com.SolucionesParaPlagas.android.Controlador.ControladorProducto;
 import com.SolucionesParaPlagas.android.Modelo.Entidad.Producto.Producto;
-import com.example.sol.R;
 
-import java.util.List;
+public class AdaptadorCarrito extends RecyclerView.Adapter<AdaptadorCarrito.CarritoViewHolder> {
 
-public class AdaptadorCarrito extends RecyclerView.Adapter<AdaptadorCarrito.ProductoViewHolder> {
+    HashMap<String, Integer> carrito = new HashMap<>();
+    private ControladorProducto controladorProducto = ControladorProducto.obtenerInstancia();
+    private Context context;
 
-    private List<Producto> listaProductos;
-
-    public AdaptadorCarrito(List<Producto> listaProductos) {
-        this.listaProductos = listaProductos;
+    public AdaptadorCarrito(HashMap<String, Integer> carrito,Context context) {
+        this.carrito = carrito;
+        this.context = context;
     }
 
     @NonNull
     @Override
-    public ProductoViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public CarritoViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         // Inflar la vista para cada elemento de la lista
-        View vista = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_prod_lista, parent, false);
-        return new ProductoViewHolder(vista);
+        View vista = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_prod_carrito, parent, false);
+        return new CarritoViewHolder(vista);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ProductoViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull CarritoViewHolder holder, int position) {
+        // Obtener el ID del producto a partir de la posición
+        String idProducto = (String) carrito.keySet().toArray()[position];
+        // Obtener la cantidad correspondiente al producto
+        int cantidad = carrito.get(idProducto);
+        // Obtener el producto usando el ID
+        Producto producto = controladorProducto.obtenerProducto(idProducto);
         // Vincular los datos con la vista
-        Producto producto = listaProductos.get(position);
         holder.nombreProducto.setText(producto.getTitle());
+        holder.cantidadProducto.setText(String.valueOf(cantidad));
+        holder.pesoProducto.setText(""+producto.getWeight());
+        String imageUrl = producto.getImageUrl();
+        ControladorImagenes controladorImagenes = new ControladorImagenes(context);
+        controladorImagenes.cargarImagenDesdeUrl(imageUrl, holder.imagenCarrito);
     }
 
     @Override
     public int getItemCount() {
-        return listaProductos.size();
+        return carrito.size();
     }
 
-    public static class ProductoViewHolder extends RecyclerView.ViewHolder {
+    public static class CarritoViewHolder extends RecyclerView.ViewHolder {
         // Referencias a los elementos de la vista
-        TextView nombreProducto;
-        public ProductoViewHolder(@NonNull View itemView) {
+        TextView nombreProducto, cantidadProducto, pesoProducto;
+        ImageView imagenCarrito;
+        public CarritoViewHolder(@NonNull View itemView) {
             super(itemView);
             nombreProducto = itemView.findViewById(R.id.nombreProducto);
+            cantidadProducto = itemView.findViewById(R.id.cantidadProducto);
+            pesoProducto = itemView.findViewById(R.id.pesoProducto);
+            imagenCarrito = itemView.findViewById(R.id.imagenProducto);
         }
     }
 
--- app/src/main/java/com/SolucionesParaPlagas/android/Vista/CarritoCompras.java
+++ app/src/main/java/com/SolucionesParaPlagas/android/Vista/CarritoCompras.java
@@ -1,8 +1,8 @@
 package com.SolucionesParaPlagas.android.Vista;
 
-import android.os.Bundle;
-import java.util.HashMap;
 import com.example.sol.R;
+import java.util.HashMap;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 import android.widget.Button;
@@ -13,32 +13,37 @@ import android.widget.ProgressBar;
 import android.content.DialogInterface;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import com.SolucionesParaPlagas.android.Controlador.Sesion;
 import com.SolucionesParaPlagas.android.Controlador.ControladorImagenes;
 import com.SolucionesParaPlagas.android.Controlador.ControladorProducto;
 import com.SolucionesParaPlagas.android.Modelo.Entidad.Producto.Producto;
 import com.SolucionesParaPlagas.android.Controlador.ControladorClienteIndividual;
 import com.SolucionesParaPlagas.android.Modelo.Entidad.Cliente.ClienteIndividual;
+import com.SolucionesParaPlagas.android.Vista.Adaptador.AdaptadorCarrito;
 
 public class CarritoCompras extends AppCompatActivity {
 
     Button btnCotizacion;
-    ImageView btnVerProductos, btnMenu, btnCerrarSesion, btnMas, btnMenos;
+    ImageView btnVerProductos, btnMenu, btnCerrarSesion;
     TextView textoCargando;
-
     private ProgressBar iconoCarga;
-    //       IDP   CantidadP
     HashMap<String, Integer> carrito = new HashMap<>();
     private ControladorImagenes controladorImagenes;
     ControladorProducto controladorProducto = ControladorProducto.obtenerInstancia();
     Sesion sesion = new Sesion();
+    private RecyclerView recyclerViewCarrito;
+    private AdaptadorCarrito adaptadorCarrito;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.carritocompra);
         inicializarElementos();
+        obtenerElementos();
         configurarBotones();
+        configurarRecyclerView();
     }
 
     private void inicializarElementos(){
@@ -48,20 +53,14 @@ public class CarritoCompras extends AppCompatActivity {
         btnCerrarSesion = findViewById(R.id.iconoCerrarSesion);
         iconoCarga = findViewById(R.id.iconoCargando);
         textoCargando = findViewById(R.id.textoCargando);
+        recyclerViewCarrito = findViewById(R.id.listaCarrito);
     }
 
     private void obtenerElementos(){
-        // Obtener el Intent que inició esta Activity
         Intent intent = getIntent();
         if(intent != null && intent.hasExtra("carrito")){
             carrito = (HashMap<String, Integer>) intent.getSerializableExtra("carrito");
         }
-    }
-
-    // Realizar esta solicutud para cada producto en un ciclo
-    private void cargarImagenProducto(String imagenUrl, ImageView imagenProducto){
-        controladorImagenes = new ControladorImagenes(this);
-        controladorImagenes.cargarImagenDesdeUrl(imagenUrl, imagenProducto);
     }
 
     private void configurarBotones() {
@@ -71,48 +70,52 @@ public class CarritoCompras extends AppCompatActivity {
         btnCotizacion.setOnClickListener(this::cotizacion);
     }
 
+    private void configurarRecyclerView() {
+        adaptadorCarrito = new AdaptadorCarrito(carrito, this);
+        recyclerViewCarrito.setLayoutManager(new LinearLayoutManager(this));
+        recyclerViewCarrito.setAdapter(adaptadorCarrito);
+    }
+
     private void regresarAProductos(View v){
         Intent intent = new Intent(CarritoCompras.this, MostrarProductos.class);
-        intent.putExtra("carrito", carrito); // Pasar el carrito de compras a la siguiente actividad
+        intent.putExtra("carrito", carrito);
         startActivity(intent);
     }
 
     private void cotizacion(View v) {
-        iconoCarga.setVisibility(View.VISIBLE);  // Mostrar ProgressBar
-        textoCargando.setVisibility(View.VISIBLE); // Mostrar Texto
+        iconoCarga.setVisibility(View.VISIBLE);
+        textoCargando.setVisibility(View.VISIBLE);
         new Thread(() -> {
             try {
                 if (mandarCorreo()) {
-                    Thread.sleep(1500); // Ajusta el tiempo según sea necesario
+                    Thread.sleep(1500);
                     runOnUiThread(() -> {
-                        iconoCarga.setVisibility(View.GONE);  // Ocultar ProgressBar
-                        textoCargando.setVisibility(View.GONE); // Ocultar Texto
-                        carrito.clear(); // Limpiamos el carrito de compras
+                        iconoCarga.setVisibility(View.GONE);
+                        textoCargando.setVisibility(View.GONE);
+                        carrito.clear();
                         notificarUsuario();
                     });
                 } else {
                     runOnUiThread(() -> {
-                        iconoCarga.setVisibility(View.GONE);  // Ocultar ProgressBar
-                        textoCargando.setVisibility(View.GONE); // Ocultar Texto
+                        iconoCarga.setVisibility(View.GONE);
+                        textoCargando.setVisibility(View.GONE);
                         Toast.makeText(this, "No se pudo enviar el correo. Inténtalo de nuevo.", Toast.LENGTH_LONG).show();
                     });
                 }
             } catch (InterruptedException e) {
                 e.printStackTrace();
                 runOnUiThread(() -> {
-                    iconoCarga.setVisibility(View.GONE);  // Ocultar ProgressBar
-                    textoCargando.setVisibility(View.GONE); // Ocultar Texto
+                    iconoCarga.setVisibility(View.GONE);
+                    textoCargando.setVisibility(View.GONE);
                     Toast.makeText(this, "Ocurrió un error durante la espera. Inténtalo de nuevo.", Toast.LENGTH_LONG).show();
                 });
             }
         }).start();
     }
 
-    // Este metodo de enviar correo ya sirve
     private boolean mandarCorreo() {
-        // Crear un intento para enviar el correo
         Intent emailIntent = new Intent(Intent.ACTION_SEND);
-        emailIntent.setType("message/rfc822");  // Tipo MIME para enviar email
+        emailIntent.setType("message/rfc822");
         emailIntent.putExtra(Intent.EXTRA_EMAIL, new String[]{"amjo220898@upemor.edu.mx"});
         emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Cotización de Productos");
         emailIntent.putExtra(Intent.EXTRA_TEXT, construirMensaje());
@@ -126,36 +129,31 @@ public class CarritoCompras extends AppCompatActivity {
     }
 
     private String construirMensaje(){
-        // Construir el cuerpo del correo con la lista de productos en el carrito
         ControladorClienteIndividual controladorClienteIndividual = ControladorClienteIndividual.obtenerInstancia();
         ClienteIndividual clienteIndividual = controladorClienteIndividual.obtenerCliente();
         StringBuilder mensajeCorreo = new StringBuilder();
         mensajeCorreo.append("Cotizacion para: "+clienteIndividual.getClientName()+",\n\n");
         mensajeCorreo.append("A continuación se muestran los productos seleccionados:\n\n");
         for (String idProducto : carrito.keySet()) {
-            Producto producto = controladorProducto.obtenerProducto(idProducto);  // Obtener producto por ID
+            Producto producto = controladorProducto.obtenerProducto(idProducto);
             if (producto != null) {
                 mensajeCorreo.append("Producto: ").append(producto.getTitle()).append("\n")
-                    .append("Cantidad: ").append(carrito.get(idProducto)).append("\n")
-                    .append("Peso: ").append(producto.getWeight()).append("\n")
-                    .append("Unidad: ").append(producto.getUnit()).append("\n")
-                    .append("-------------------------\n");
+                        .append("Cantidad: ").append(carrito.get(idProducto)).append("\n")
+                        .append("Peso: ").append(producto.getWeight()).append("\n")
+                        .append("Unidad: ").append(producto.getUnit()).append("\n")
+                        .append("-------------------------\n");
             }
         }
         if(clienteIndividual.getPhone() != null && clienteIndividual.getEmail() != null){
-            // Caso para cuando no es nulo ni el correo ni el telefono
             mensajeCorreo.append("\nConfirmar cotizacion al correo: "+clienteIndividual.getEmail()+
                     "\n\no al telefono: "+clienteIndividual.getPhone()+"\n\nSoluciones Para Plagas");
         }else if(clienteIndividual.getEmail() != null && clienteIndividual.getPhone() == null){
-            // Caso para cuando el correo existe pero el telefono no
             mensajeCorreo.append("\nConfirmar cotizacion al correo: "
                     +clienteIndividual.getEmail()+"\n\nSoluciones Para Plagas");
         }else if(clienteIndividual.getEmail() == null && clienteIndividual.getPhone() != null){
-            // Caso para cuando existe el telefono pero el correo no
             mensajeCorreo.append("\nConfirmar cotizacion al telefono: "
                     +clienteIndividual.getPhone()+"\n\nSoluciones Para Plagas");
         }else{
-            // Caso cuando el cliente no cuenta ni con telefono ni correo
             mensajeCorreo.append("\nSe le confirmara la cotizacion al remitente de este correo: "
                     +"\n\nSoluciones Para Plagas");
         }
@@ -179,16 +177,16 @@ public class CarritoCompras extends AppCompatActivity {
 
     private void notificarUsuario(){
         new AlertDialog.Builder(this)
-            .setTitle("Aviso")
-            .setMessage("¡Gracias por tu preferencia! Pronto, uno de nuestros asistentes te contactará con la cotización de tus productos solicitados.")
-            .setPositiveButton("OK",new DialogInterface.OnClickListener(){
-                @Override
-                public void onClick(DialogInterface dialog, int which){
-                    Intent intent = new Intent(CarritoCompras.this, MenuPrincipal.class);
-                    startActivity(intent);
-                }
-            })
-            .show();
+                .setTitle("Aviso")
+                .setMessage("¡Gracias por tu preferencia! Pronto, uno de nuestros asistentes te contactará con la cotización de tus productos solicitados.")
+                .setPositiveButton("OK",new DialogInterface.OnClickListener(){
+                    @Override
+                    public void onClick(DialogInterface dialog, int which){
+                        Intent intent = new Intent(CarritoCompras.this, MenuPrincipal.class);
+                        startActivity(intent);
+                    }
+                })
+                .show();
     }
 
 }
