7a331d14cece3c157b214fddd749be89d5344705
JesusArizmendi <arizmendimj04@gmail.com>
2024-10-20 13:29:01 -0600

    Proyecto

---
+++ app/src/main/java/com/SolucionesParaPlagas/android/Controlador/ControladorCliente.java
@@ -0,0 +1,124 @@
+package com.SolucionesParaPlagas.android.Controlador;
+
+import java.sql.SQLException;
+import android.content.Context;
+import com.SolucionesParaPlagas.android.Modelo.Entidad.Cliente;
+import com.SolucionesParaPlagas.android.Modelo.Repositorio.RepositorioCliente;
+
+public class ControladorCliente extends Controlador<Cliente>{
+
+    private static ControladorCliente instancia;
+
+    public ControladorCliente(Context context) {
+        super(RepositorioCliente.obtenerInstancia(), context);
+        nameTable = "cliente";
+    }
+
+    public static ControladorCliente obtenerInstancia(Context context) {
+        if (instancia == null) {
+            instancia = new ControladorCliente(context);
+        }
+        return instancia;
+    }
+
+    @Override
+    protected boolean insertObject(Cliente cliente) {
+        String query = "INSERT INTO " + nameTable + " VALUES ("
+                + "0, " // Id con zerofill y auto increment
+                + "'" + cliente.getClienteRFC() + "', "
+                + "'" + cliente.getNombreC() + "', "
+                + "'" + cliente.getRazonSocial() + "', "
+                + "'" + cliente.getEmail() + "', "
+                + "'" + cliente.getTelefonoC() + "', "
+                + "'" + cliente.getCalle() + "', "
+                + "'" + cliente.getColonia() + "', "
+                + "'" + cliente.getLocalidad() + "', "
+                + "'" + cliente.getMunicipio() + "', "
+                + "'" + cliente.getEstado() + "', "
+                + cliente.getClienteCP() + ", "
+                + cliente.getDiasCredito() + ", "
+                + cliente.getMontoCredito() + ", "
+                + "'" + cliente.getUrlFotoPerfil() + "');";
+        return ejecutarActualizacion(query);
+    }
+
+    @Override
+    protected boolean deleteObject(int id) {
+        String query = "DELETE FROM " + nameTable + " WHERE noCliente = " + id + ";";
+        return ejecutarActualizacion(query);
+    }
+
+    @Override
+    protected boolean updateObject(Cliente cliente) {
+        String query = "UPDATE " + nameTable + " SET "
+                + "clienteRFC = '" + cliente.getClienteRFC() + "', "
+                + "nombreC = '" + cliente.getNombreC() + "', "
+                + "razonSocial = '" + cliente.getRazonSocial() + "', "
+                + "email = '" + cliente.getEmail() + "', "
+                + "telefonoC = '" + cliente.getTelefonoC() + "', "
+                + "calle = '" + cliente.getCalle() + "', "
+                + "colonia = '" + cliente.getColonia() + "', "
+                + "localidad = '" + cliente.getLocalidad() + "', "
+                + "municipio = '" + cliente.getMunicipio() + "', "
+                + "estado = '" + cliente.getEstado() + "', "
+                + "clienteCP = " + cliente.getClienteCP() + ", "
+                + "diasCredito = " + cliente.getDiasCredito() + ", "
+                + "montoCredito = " + cliente.getMontoCredito() + ", "
+                + "urlFotoPerfil = '" + cliente.getUrlFotoPerfil() + "' "
+                + "WHERE noCliente = " + cliente.getNoCliente() + ";";
+        return ejecutarActualizacion(query);
+    }
+
+    @Override
+    protected Cliente getObject(int id) {
+        String query = "SELECT * FROM " + nameTable + " WHERE noCliente = " + id + ";";
+        conector.registro = ejecutarConsulta(query);
+        return BDToObject(conector);
+    }
+
+    // Metodo no usable
+    @Override
+    protected Cliente getObject(String campo) {
+        return null;
+    }
+
+    @Override
+    protected Cliente getObject(String RFC, String telefono) {
+        limpiarRepositorio();
+        String query = "SELECT * FROM " + nameTable + " WHERE clienteRFC = '" + RFC + "' and telefonoC = '" + telefono + "';";
+        conector.registro = ejecutarConsulta(query);
+        repositorio.setObjeto(BDToObject(conector));
+        return repositorio.getObjeto();
+    }
+
+    @Override
+    protected Cliente BDToObject(Conector conector) {
+        if (conector.registro != null) {
+            try {
+                if (conector.registro.next()) {
+                    Cliente cliente = new Cliente();
+                    cliente.setNoCliente(conector.registro.getInt("noCliente"));
+                    cliente.setClienteRFC(conector.registro.getString("clienteRFC"));
+                    cliente.setNombreC(conector.registro.getString("nombreC"));
+                    cliente.setRazonSocial(conector.registro.getString("razonSocial"));
+                    cliente.setEmail(conector.registro.getString("email"));
+                    cliente.setTelefonoC(conector.registro.getString("telefonoC"));
+                    cliente.setCalle(conector.registro.getString("calle"));
+                    cliente.setColonia(conector.registro.getString("colonia"));
+                    cliente.setLocalidad(conector.registro.getString("localidad"));
+                    cliente.setMunicipio(conector.registro.getString("municipio"));
+                    cliente.setEstado(conector.registro.getString("estado"));
+                    cliente.setClienteCP(conector.registro.getInt("clienteCP"));
+                    cliente.setDiasCredito(conector.registro.getInt("diasCredito"));
+                    cliente.setMontoCredito(conector.registro.getFloat("montoCredito"));
+                    cliente.setUrlFotoPerfil(conector.registro.getString("urlFotoPerfil"));
+                    return cliente;
+                }
+            } catch (SQLException ex) {
+                manejarExcepcion(ex);
+            }
+        }
+        return null;
+    }
+
+}
---
+++ app/src/main/java/com/SolucionesParaPlagas/android/Controlador/ControladorValidaciones.java
@@ -0,0 +1,146 @@
+package com.SolucionesParaPlagas.android.Controlador;
+
+import android.widget.Toast;
+import android.content.Intent;
+import java.util.regex.Pattern;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import com.SolucionesParaPlagas.android.Vista.PaginaInicio;
+
+public class ControladorValidaciones {
+
+    // Expresiones regulares para validar RFCs
+    private static final String RFCPersonaFisica = "^[A-ZÑ&]{4}\\d{6}[A-Z0-9]{3}$";
+    private static final String RFCPersonaMoral = "^[A-ZÑ&]{3}\\d{6}$";
+
+    /**
+     * Valida un RFC.
+     *
+     * @param RFC El RFC a validar.
+     * @return true si el RFC es válido, false en caso contrario.
+     */
+    public boolean validarRFC(String RFC) {
+        if (RFC == null || RFC.isEmpty()) {
+            return false;
+        }
+        // Normaliza el RFC a mayúsculas
+        RFC = RFC.toUpperCase();
+        // Valida el RFC según el formato
+        if (RFC.length() == 12) {
+            return true;
+        } else if (RFC.length() == 13) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Valida un RFC para persona física.
+     *
+     * @param RFC El RFC a validar.
+     * @return true si el RFC es válido, false en caso contrario.
+     */
+    private boolean validarRFCFisica(String RFC) {
+        return Pattern.matches(RFCPersonaFisica, RFC);
+    }
+
+    /**
+     * Valida un RFC para persona moral.
+     *
+     * @param RFC El RFC a validar.
+     * @return true si el RFC es válido, false en caso contrario.
+     */
+    private boolean validarRFCMoral(String RFC) {
+        return Pattern.matches(RFCPersonaMoral, RFC);
+    }
+
+    /**
+     * Valida que un campo contenga solo números.
+     *
+     * @param campo El campo a validar.
+     * @return true si el campo contiene solo números, false en caso contrario.
+     */
+    public boolean validarSoloNumeros(String campo) {
+        for (int i = 0; i < campo.length(); i++) {
+            if (!Character.isDigit(campo.charAt(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean validarStringVacio(String campo){
+        if(campo.isEmpty()){
+            // Si el campo está vacío, devuelve true
+            return true;
+        }
+        // Si el campo no está vacío, devuelve false
+        return false;
+    }
+
+    public boolean validarStringNull(String campo){
+        if(campo == null){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public String capitalizarLetras(String texto) {
+        if (texto == null || texto.isEmpty()) {
+            return texto;
+        }
+        StringBuilder resultado = new StringBuilder();
+        boolean capitalizar = true;
+        for (char c : texto.toCharArray()) {
+            if (Character.isWhitespace(c)) {
+                capitalizar = true;
+                resultado.append(c);
+            } else if (capitalizar) {
+                resultado.append(Character.toUpperCase(c));
+                capitalizar = false;
+            } else {
+                resultado.append(Character.toLowerCase(c));
+            }
+        }
+        return resultado.toString();
+    }
+
+    // Método para mostrar un cuadro de diálogo y confirmar si el usuario quiere cerrar la sesión
+    public void confirmarCerrarSesion(Context context) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setMessage("¿Estás seguro de que deseas cerrar sesión?")
+                .setPositiveButton("Sí", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // Si el usuario confirma, se limpia la sesión
+                        cerrarSesion(context);
+                        Toast.makeText(context, "Sesión cerrada", Toast.LENGTH_SHORT).show();
+                        irALogin(context); // Redirigir al login
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // No hacer nada si el usuario cancela
+                        dialog.dismiss();
+                    }
+                });
+        // Mostrar el cuadro de diálogo
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+    private void cerrarSesion(Context contexto){
+        ControladorCliente controladorCliente = ControladorCliente.obtenerInstancia(contexto);
+        ControladorProducto controladorProducto = ControladorProducto.obtenerInstancia(contexto);
+        controladorCliente.limpiarRepositorio();
+        controladorProducto.limpiarRepositorio();
+    }
+
+    // Redirigir al login
+    private void irALogin(Context context) {
+        Intent intent = new Intent(context, PaginaInicio.class);
+        context.startActivity(intent);
+    }
+
+}
